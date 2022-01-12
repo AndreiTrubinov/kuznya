65f09cd567136e3ca6fc578d430c2ceca034f6ec
AndreiTrubinov <hiddengard@gmail.com>
2022-01-12 20:53:22 +0500

    need to add original images and retina support

---
+++ app/sendmail.php
@@ -0,0 +1,49 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('ru', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+	//От кого письмо
+	$mail->setFrom('kudymkar@mail.xyz', 'Кузница-сайт');
+	//Кому отправить
+	$mail->addAddress('hiddengard@gmail.com');
+	//Тема письма
+	$mail->Subject = 'Кузница - Новое сообщение';
+
+
+	//Тело письма
+	$body = '<h1>Информация пользователя</h1>';
+	
+	if(trim(!empty($_POST['name']))){
+		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+	}
+	if(trim(!empty($_POST['phone']))){
+		$body.='<p><strong>Телефон:</strong> '.$_POST['phone'].'</p>';
+	}
+	if(trim(!empty($_POST['message']))){
+		$body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
+	}
+	
+	
+
+	$mail->Body = $body;
+
+	//Отправляем
+	if (!$mail->send()) {
+		$message = 'Ошибка';
+	} else {
+		$message = 'Данные отправлены!';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
